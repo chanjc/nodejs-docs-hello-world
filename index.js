972b6f06ae3e7cc548cbfc23fb50d3713cc7f6a3
Chan Jen Chyi <chanjch@my.ibm.com>
2017-05-30 13:51:37 +0800

    Test error deployment
    Create issue to trigger error/failure deployment

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!");
+    response.end("Not working!!!");
 
 });
-
-var port = process.env.PORT || 1337;
+// Try to screw up the deployment
+// var port = process.env.PORT || 8080;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
