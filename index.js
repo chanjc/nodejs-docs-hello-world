1c84013e4b01bb69ca9373ecb5e980f68c873ba3
Chan Jen Chyi <chanjch@my.ibm.com>
2017-05-30 14:20:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Not working!!!");
+    response.end("test deploy!!!");
 
 });
 // Try to screw up the deployment
-// var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
